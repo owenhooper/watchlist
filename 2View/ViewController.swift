3c4933d66499833a54c56ee6080cb488a2fe876c
Owen Hooper <Owen@owens-macbook-pro.local>
2016-02-17 12:15:55 -0800

    Initial Commit

---
+++ 2View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2View
+//
+//  Created by Owen Hooper on 2016-02-17.
+//  Copyright © 2016 Owen Hooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
